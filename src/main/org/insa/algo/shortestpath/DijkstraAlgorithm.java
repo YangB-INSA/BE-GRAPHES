85b935f3d0d8ba65c80d46a5741082bb6475c4b0
cloury <cloury.pierre@gmail.com>
2019-05-04 13:06:43 +0200

    avancement dijkstra

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -1,4 +1,7 @@
 package org.insa.algo.shortestpath;
+import org.insa.graph.*;
+import java.util.*;
+import org.insa.algo.utils.*;
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
@@ -8,9 +11,51 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     @Override
     protected ShortestPathSolution doRun() {
+    	
+    	
         ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        // TODO:
+        
+        
+        
+        Graph graph = data.getGraph();
+        int size_graph = graph.size();
+        List<Node> nodes = graph.getNodes();
+        
+        BinaryHeap<Label> bin_heap = new BinaryHeap<Label>();
+        List<Label> list_label = new ArrayList<Label>();
+        
+        Node node_origin = data.getOrigin();
+        Node node_destination = data.getDestination();
+        int nb_node_marque = 1;
+        
+        
+        // phase d'initialisation
+        for (Node node : nodes)
+        {
+        	boolean marque = false;
+        	int cout = 10000;
+        	if (node.getId()==node_origin.getId())
+        	{
+        		marque = true;
+        		cout = 0;
+        		Label label_origin = new Label(node.getId(),marque,cout,0);
+        		bin_heap.insert(label_origin);
+        		
+        	}
+        	Label label = new Label(node.getId(),marque,cout,0);
+        	list_label.add(label);
+        }
+        
+        
+        
+        //algorithme
+        while (nb_node_marque < size_graph)
+        {
+        	Label x = bin_heap.findMin();
+        	
+        }
+        
         return solution;
     }
 
