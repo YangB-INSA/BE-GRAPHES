37db37fa72b3c9bb0d9871cfecb025b842149a1a
cloury <cloury.pierre@gmail.com>
2019-05-06 09:39:39 +0200

    avancement dijkstra 3

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -31,6 +31,7 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         
         
         // phase d'initialisation
+        /*
         for (Node node : nodes)
         {
         	boolean marque = false;
@@ -50,20 +51,24 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         		list_label.add(label);
         	}
         }
+        */
         
-        Collections.sort(list_label, Label.ComparatorSommet);
-        
-        
+        //insertion du premier label
+        Label label_origin = new Label(node_origin.getId(),true,0,0);
+        list_label.set(node_origin.getId(), label_origin);
+        bin_heap.insert(label_origin);
         
         //algorithme
         while (nb_node_marque < size_graph)
         {
         	//on trouve le plus petit element du tas
-        	Label x = bin_heap.findMin();
+        	Label x = bin_heap.deleteMin();
         	Node node_x = graph.get(x.getSommet());
-        	//cette element devient "marqué"
+        	//cet element devient "marqué"
         	x.marque = true;
         	list_label.set(node_x.getId(), x);
+        	
+        	nb_node_marque ++;
         	
         	//cout de cette element
         	double cost_x = x.getCost();
@@ -74,6 +79,18 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         	{
         		Node node_y = arc_successor.getDestination();
         		Label label_y = list_label.get(node_y.getId());
+        		
+        		boolean exist_prev = true;
+        		
+        		if (label_y == null)
+        		{
+        			exist_prev = false;
+        			Label init_label_y = new Label(node_y.getId(),false,1e10,0);
+        			list_label.set(node_y.getId(), init_label_y);
+        		}
+        		
+        		
+        		
         		//si un successeur n'est pas marqué
         		if (label_y.marque == false)
         		{
@@ -81,11 +98,15 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         			if (cost_y > (cost_x + arc_successor.getMinimumTravelTime()))
 					{
         				double new_cost = cost_x + arc_successor.getMinimumTravelTime();
-						Label new_label_y = new Label(node_y.getId(),false,new_cost,x.getSommet()); 
+						Label new_label_y = new Label(node_y.getId(),false,new_cost,x.getSommet());
+						if (exist_prev == true)
+						{
+							bin_heap.remove(label_y);
+						}
+						
 						list_label.set(node_y.getId(), new_label_y);
 						bin_heap.insert(new_label_y);
-						nb_node_marque ++;
-					}	
+					}
         				
         		}
         		
