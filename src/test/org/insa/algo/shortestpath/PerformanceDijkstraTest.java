f8399b91f472755287b015fa9e74b9d648412524
Boris-INSA <borisyang1997@gmail.com>
2019-05-26 14:34:16 +0200

    version a peu près bonne du test de perf

--- src/test/org/insa/algo/shortestpath/PerformanceDijkstraTest.java
+++ src/test/org/insa/algo/shortestpath/PerformanceDijkstraTest.java
@@ -14,9 +14,9 @@ import org.junit.Test;
 
 public class PerformanceDijkstraTest {
     
-	public static Graph toulouseGraph;
+	public static Graph franceGraph;
 	
-	public static Graph haitiGraph;
+	public static Graph belgiumGraph;
 	
 	public static Graph garonneGraph;
 	
@@ -26,31 +26,31 @@ public class PerformanceDijkstraTest {
 	
 	public static ShortestPathData data1,data2,data3,data4,data5,data6;
 	
-	public static long Time1,Time2;
+	public static long Time1,Time2,Time3;
 
 	@BeforeClass
 	public static void initAll() throws Exception {
 		
 		//cartes utilisées
 		//String mapToulouse = "/home/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/toulouse.mapgr";
-		String mapToulouse = "D:\\Projet INSA\\Maps\\toulouse.mapgr";
+		String mapFrance = "D:\\Projet INSA\\Maps\\france.mapgr";
 		//String mapHaiti = "/home/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/haiti-and-domrep.mapgr";
-		String mapHaiti = "D:\\Projet INSA\\Maps\\haiti-and-domrep.mapgr";
+		String mapBelgium = "D:\\Projet INSA\\Maps\\belgium.mapgr";
 		//String mapBelgium = "/home/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/belgium.mapgr";
 		String mapGaronne = "D:\\Projet INSA\\Maps\\haute-garonne.mapgr";
 		
         GraphReader reader1 = new BinaryGraphReader(
-                new DataInputStream(new BufferedInputStream(new FileInputStream(mapToulouse))));
+                new DataInputStream(new BufferedInputStream(new FileInputStream(mapFrance))));
         
         GraphReader reader2 = new BinaryGraphReader(
-                new DataInputStream(new BufferedInputStream(new FileInputStream(mapHaiti))));
+                new DataInputStream(new BufferedInputStream(new FileInputStream(mapBelgium))));
         
         GraphReader reader3 = new BinaryGraphReader(
                 new DataInputStream(new BufferedInputStream(new FileInputStream(mapGaronne))));
         
         // Graph de test
-		toulouseGraph = reader1.read();
-		haitiGraph = reader2.read();
+		franceGraph = reader1.read();
+		belgiumGraph = reader2.read();
 		garonneGraph = reader3.read();
 		
 		//arc inspector
@@ -58,10 +58,10 @@ public class PerformanceDijkstraTest {
 		arcinspectortime = ArcInspectorFactory.getAllFilters().get(2);
 		
 		//data length
-		data1 = new ShortestPathData(toulouseGraph,toulouseGraph.get(17161),toulouseGraph.get(23752),arcinspectorlength);
+		data1 = new ShortestPathData(belgiumGraph,belgiumGraph.get(350502),belgiumGraph.get(230969),arcinspectorlength);
 		data2 = new ShortestPathData(garonneGraph,garonneGraph.get(26431),garonneGraph.get(60105),arcinspectorlength);
 		//data time
-		data3 = new ShortestPathData(toulouseGraph,toulouseGraph.get(17161),toulouseGraph.get(23752),arcinspectortime);
+		data3 = new ShortestPathData(belgiumGraph,belgiumGraph.get(350502),belgiumGraph.get(230969),arcinspectortime);
 		data4 = new ShortestPathData(garonneGraph,garonneGraph.get(26431),garonneGraph.get(60105),arcinspectortime);
 	}
 	
@@ -74,51 +74,77 @@ public class PerformanceDijkstraTest {
 		DijkstraAlgorithm dij2 = new DijkstraAlgorithm(data2);
 		AStarAlgorithm star2 = new AStarAlgorithm(data2);
 		
+		System.out.println("\n ------Test ShortestPath------ \n" );
 		//A tester sur de grandes cartes pour voir des résultats significatifs
-		System.out.println("Test sur la carte de Toulouse \n" );
+		System.out.println("Test sur la carte de Belgique \n" );
 		
 		Time1 = System.currentTimeMillis();
 		dij.doRun();
 		Time2 = System.currentTimeMillis() - Time1;
-		System.out.println("Temps d'execution Dijkstra :" + Time2 );
+		System.out.println("Temps d'execution Dijkstra :" + Time2 +" ms");
 		
 		Time1 = System.currentTimeMillis();
 		star.doRun();
-		Time2 = System.currentTimeMillis() - Time1;
-		System.out.println("Temps d'execution A*:" + Time2 );
+		Time3 = System.currentTimeMillis() - Time1;
+		System.out.println("Temps d'execution A*:" + Time3 +" ms");
+		System.out.println("A* est " + (float)Time2/Time3 + " plus rapide que Dijkstra" );
+		
 		
 		System.out.println("\nTest sur la carte de Haute-Garonne \n" );
 		
 		Time1 = System.currentTimeMillis();
 		dij2.doRun();
 		Time2 = System.currentTimeMillis() - Time1;
-		System.out.println("Temps d'execution Dijkstra :" + Time2 );
+		System.out.println("Temps d'execution Dijkstra :" + Time2 +" ms");
 		
 		Time1 = System.currentTimeMillis();
 		star2.doRun();
-		Time2 = System.currentTimeMillis() - Time1;
-		System.out.println("Temps d'execution A* :" + Time2 );
-		
-		
+		Time3 = System.currentTimeMillis() - Time1;
+		System.out.println("Temps d'execution A* :" + Time3 +" ms");
+		System.out.println("A* est " + (float)Time2/Time3 + " plus rapide que Dijkstra" );
+
 	}
 	
-	/*@Test 
+	@Test 
 	public void testTemps() 
 	{
 		
 		DijkstraAlgorithm dij = new DijkstraAlgorithm(data3);
 		AStarAlgorithm star = new AStarAlgorithm(data3);
-		BellmanFordAlgorithm bell = new BellmanFordAlgorithm(data3);
+		
 		DijkstraAlgorithm dij2 = new DijkstraAlgorithm(data4);
 		AStarAlgorithm star2 = new AStarAlgorithm(data4);
-		BellmanFordAlgorithm bell2 = new BellmanFordAlgorithm(data4);
 		
-		//test dijkstra
-		assertEquals(dij.doRun().getPath().getLength(),star.doRun().getPath().getMinimumTravelTime()(),0);
-		assertEquals(dij2.doRun().getPath().getLength(),star2.doRun().getPath().getMinimumTravelTime()(),0);
-			
+		System.out.println("\n ------Test FastestPath------ \n" );
+		//A tester sur de grandes cartes pour voir des résultats significatifs
+		System.out.println("\nTest sur la carte de Belgique \n" );
+		
+		Time1 = System.currentTimeMillis();
+		dij.doRun();
+		Time2 = System.currentTimeMillis() - Time1;
+		System.out.println("Temps d'execution Dijkstra :" + Time2 +" ms");
+		
+		Time1 = System.currentTimeMillis();
+		star.doRun();
+		Time3 = System.currentTimeMillis() - Time1;
+		System.out.println("Temps d'execution A*:" + Time3 +" ms");
+		System.out.println("A* est " + (float)Time2/Time3 + " plus rapide que Dijkstra" );
+		
+		
+		System.out.println("\nTest sur la carte de Haute-Garonne \n" );
+		
+		Time1 = System.currentTimeMillis();
+		dij2.doRun();
+		Time2 = System.currentTimeMillis() - Time1;
+		System.out.println("Temps d'execution Dijkstra :" + Time2 +" ms");
+		
+		Time1 = System.currentTimeMillis();
+		star2.doRun();
+		Time3 = System.currentTimeMillis() - Time1;
+		System.out.println("Temps d'execution A* :" + Time3 +" ms");
+		System.out.println("A* est " + (float)Time2/Time3 + " plus rapide que Dijkstra" );
 	}
-*/
+
 }
 
 
